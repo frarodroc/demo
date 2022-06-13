f11a7612fae2794b9f34360dccd1af81b0eccafe
frarodroc <107277617+frarodroc@users.noreply.github.com>
2022-06-13 12:16:24 +0100

    Read password from system env variable

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,8 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		String password = System.getProperty("PASSWORD");
+		return "My password is: " + password;
 	}
 
 }
