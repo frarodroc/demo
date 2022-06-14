c739a95b1e44d864678224e8c6afb9430d9b57a4
frarodroc <107277617+frarodroc@users.noreply.github.com>
2022-06-14 12:31:50 +0100

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,8 +1,12 @@
 package com.example.springboot;
 
+import org.springframework.stereotype.*
+import org.springframework.beans.factory.annotation.*
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 @RestController
 public class HelloController {
 
